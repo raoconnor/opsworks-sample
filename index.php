8e9549d5fe2c36e40d1fe2239cff21273a701691
raoconnor <raoconnor@users.noreply.github.com>
2018-01-29 23:29:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <div align="center">
                 <h1>Hello World</h1>
                 <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
-                <div align="center"><img src="https://s3.eu-west-2.amazonaws.com/raoconnor/goldengate.jpg"></div>
+                <div align="center"><img src="https://s3.eu-west-2.amazonaws.com/raoconnor/goldengate.jpg" style="width:400px;height:300px;></div>
                 <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
